de6348e51d0526c6d112c7a9cbc05a203def051b
Andrew Cheung <90213388+andrewcheung27@users.noreply.github.com>
2025-03-13 12:51:01 -0700

    reverted back to old gun. put old gun back in levels 1-3

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -5,7 +5,7 @@ public class Weapon : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint; // Assign a child empty GameObject where bullets spawn
     public float bulletSpeed = 20f;
-    
+
     public void Shoot(Vector3 direction)
     {
         // can't shoot if game is over
@@ -13,7 +13,8 @@ public class Weapon : MonoBehaviour
             return;
         }
 
-        GameObject bulletInstance = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        GameObject bulletInstance = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        bulletInstance.transform.Rotate(new Vector3(90f, 0f, 0f));  // make bullet horizontal
         Bullet bulletScript = bulletInstance.GetComponent<Bullet>();
         if (bulletScript != null)
         {
