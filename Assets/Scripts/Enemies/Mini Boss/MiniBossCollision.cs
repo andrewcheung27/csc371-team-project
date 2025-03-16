3cdfb606b0de5ef7c99ccbd49f3ed959edf74099
Andrew Cheung <90213388+andrewcheung27@users.noreply.github.com>
2025-03-16 11:19:26 -0700

    removed knockback on melee attack and improved hitbox

--- Assets/Scripts/Enemies/Mini Boss/MiniBossCollision.cs
+++ Assets/Scripts/Enemies/Mini Boss/MiniBossCollision.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class MiniBossCollision : MonoBehaviour
 {
+    public int damage = 1;
+
     // This method is called when the miniboss collides with something
     private void OnCollisionEnter(Collision collision)
     {
@@ -12,18 +14,18 @@ public class MiniBossCollision : MonoBehaviour
             // Apply damage using the GameManager (assuming GameManager has a method to deal damage)
             if (GameManager.instance != null)
             {
-                GameManager.instance.AddToHealth(-1); // Reduce player health by 1
+                GameManager.instance.AddToHealth(-damage);
             }
 
-            // Optionally, you could apply some force to the player or other effects
-            // For example, you can add a small knockback effect if you want:
-            Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
-            if (playerRigidbody != null)
-            {
-                // Apply a knockback force (optional)
-                Vector3 knockbackDirection = (collision.transform.position - transform.position).normalized;
-                playerRigidbody.AddForce(knockbackDirection * 5f, ForceMode.Impulse);
-            }
+            // // Optionally, you could apply some force to the player or other effects
+            // // For example, you can add a small knockback effect if you want:
+            // Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            // if (playerRigidbody != null)
+            // {
+            //     // Apply a knockback force (optional)
+            //     Vector3 knockbackDirection = (collision.transform.position - transform.position).normalized;
+            //     playerRigidbody.AddForce(knockbackDirection * 5f, ForceMode.Impulse);
+            // }
         }
     }
 }
