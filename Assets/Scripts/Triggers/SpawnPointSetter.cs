f4d336de9a28e5c144cee5acce638fb6fc55c6b5
Andrew Cheung <90213388+andrewcheung27@users.noreply.github.com>
2025-03-18 11:46:33 -0700

    made spawn point setters only work once, so you can't get your spawn set to a previous checkpoint

--- Assets/Scripts/Triggers/SpawnPointSetter.cs
+++ Assets/Scripts/Triggers/SpawnPointSetter.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class SpawnPointSetter : MonoBehaviour
 {
+    public bool canSetSpawnPoint = true;
+
     void OnTriggerEnter(Collider other)
     {
         // when player enters this trigger, set spawn point to here
-        if (other.gameObject.CompareTag("Player")) {
+        if (canSetSpawnPoint && other.gameObject.CompareTag("Player")) {
             GameManager.instance.SetSpawnPoint(transform);
+            canSetSpawnPoint = false;
         }
     }
 }
